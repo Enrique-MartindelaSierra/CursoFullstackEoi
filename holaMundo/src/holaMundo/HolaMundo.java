5885ab506a260e96b69e50f99bf5d1be96168096
Enrique-MartindelaSierra <enriquemdlse22@gmail.com>
2023-01-17 12:28:40 +0100

    Update HolaMundo.java

--- holaMundo/src/holaMundo/HolaMundo.java
+++ holaMundo/src/holaMundo/HolaMundo.java
@@ -1,10 +1,41 @@
 package holaMundo;
 
+/**
+ * primera clase del curso
+ * @author Enrique
+ * @since 1
+ */
+ 
 public class HolaMundo {
-
-	public static void main(String[] args) {
+    
+	public static void ejemplosComentarios () {
+		
+		
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+		/* 
+    System.out.println("Hola Mundo a");
+	System.out.println("Hola Mundo a");
 	System.out.println("Hola Mundo");
-
+	System.out.println("Hola Mundo");
+	System.out.println("Hola Mundo a");
+	System.out.println("Hola Mundo a");*/
+		System.out.println("Hola Mundo");
+		
+		
+	}
+	
+	private static void ejemploTipos() {
+		int numero = 5;
+		int numero2 = 7;
+		int suma = numero + numero2;
+		System.out.println(suma);
+	}
+	
+	public static void main(String[] args) {
+		ejemplosComentarios();
+		ejemploTipos();
 	}
 
 }
